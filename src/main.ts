0bfcbd79896061e4ae1bf7847e896ac091ab341d
Natanael Emilio da Costa <48284698+NatanaelEmilioC@users.noreply.github.com>
2021-06-10 20:54:19 -0300

    abertura do origin para requisições recebidas

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  /* origin está aberta para todos somente em carater de desenvolvimento e teste */
+  app.enableCors({
+    origin: '*',
+  });
   await app.listen(3000);
 }
+
 bootstrap();
